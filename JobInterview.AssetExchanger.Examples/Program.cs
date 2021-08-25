8a2d18ba182423e2de17cbddf0a84c713d4f3d87
ppetrov-dev <pppetrov92@gmail.com>
2021-08-25 18:49:59 +0300

    xJI-2 - Add examples of Asset Exchanger usages. Introduce empty JobInterview.AssetExchanger.Examples and JobInterview.AssetExchanger.Examples.Tests projects.

---
+++ JobInterview.AssetExchanger.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobInterview.AssetExchanger.Examples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
