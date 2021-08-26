6687d08be20443906ea5e01c7c9bd217fff723e0
ppetrov-dev <pppetrov92@gmail.com>
2021-08-26 15:08:54 +0300

    xJI-2 - Add examples of Asset Exchanger usages. Add TestUtils

---
+++ UnitTests/JobInterview.TestUtils/CancellationTokenSourceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace JobInterview.TestUtils
+{
+    public static class CancellationTokenSourceExtensions
+    {
+        public static CancellationTokenSourceAssertions Should(this CancellationTokenSource cancellationTokenSource)
+        {
+            return new(cancellationTokenSource);
+        }
+    }
+}
